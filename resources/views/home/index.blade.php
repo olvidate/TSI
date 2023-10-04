cc0f50f283e47c7a21391a40abc7f7b8c13537ec
benjamin-ortizc <benjamin.ortizc@icloud.com>
2023-10-04 01:09:15 -0300

    feat: update views using native css

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,11 +1,88 @@
 @extends('templates.master')
 
+@section('head-extras')
+    <style>
+        .products-grid {
+            width: 100%;
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(22%, 1fr));
+            gap: 20px;
+            @media (max-width: 600px) {
+                grid-template-columns: repeat(auto-fill, minmax(100%, 1fr)); /* Ocupar todo el ancho en dispositivos móviles */
+            }
+        }
+
+        .product-card {
+            display: flex;
+            flex-direction: column;
+            padding: 2%;
+            background-color: #d1d1e9;
+            /*border: 1px rgba(0,0,0,0.5) solid;*/
+            border-radius: 3px;
+            margin-bottom: 20px;
+            position: relative;
+        }
+
+        .product-card .asd {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 10%;
+            height: 5%;
+            color: white;
+            background-color: darkgreen;
+            position: absolute;
+        }
+
+        .product-image img {
+            width: 100%;
+        }
+
+        .product-info {
+            /*margin-top: auto;*/
+            margin: 0;
+            text-align: center;
+        }
+
+        .product-info > h5 {
+            padding: .4rem;
+            font-size: 1rem;
+            color: #2b2c34;
+        }
+
+        .product-info > h5 > a {
+            color: forestgreen;
+        }
+
+        img {
+            mix-blend-mode: multiply;
+        }
+    </style>
+@endsection
+
 @section('main-content')
     @if(!empty(Auth::user()->rut_cliente))
         {{Auth::user()->rut_cliente}}
     @endif
 
-    @foreach($productos as $producto)
-        {{$producto->cod_producto}}
-    @endforeach
+    <main>
+        <section class="products-grid">
+            @foreach($productos as $producto)
+                <div class="product-card">
+
+                    <div class="asd">
+                        <small>+</small>
+                    </div>
+
+                    <div class="product-image">
+                        <img src="https://www.terra-ws.cl/26-home_default/cono-de-senalizacion-vial-28-base-negra.jpg">
+                    </div>
+                    <div class="product-info">
+                        <h5>Cono de Vial de Seguridad 28 base negra</h5>
+                        <h5><a href="#">Cotizar</a></h5>
+                    </div>
+                </div>
+            @endforeach
+        <section>
+    </main>
 @endsection
--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -5,22 +5,86 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+{{--    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">--}}
     <title>Aragun LTDA</title>
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            background-color: #fff;
+            overflow-x: hidden;
+            font-family: Inter, sans-serif;
+            font-weight: bold;
+        }
+
+        header {
+            width: 100%;
+            height: 8vh;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0 10%;
+            color: white;
+            margin-bottom: 1vw;
+        }
+
+        header > h1 > a {
+            color: #2b2c34;
+        }
+
+        header > ul {
+            display: flex;
+            text-align: end;
+            list-style: none;
+            align-items: center;
+        }
+
+        header > ul > li {
+            padding: .8vw;
+        }
+
+        header > ul > li a {
+            font-size: 1.2rem;
+        }
+
+        a {
+            text-decoration: none;
+            color: #2b2c34;
+        }
+
+        a:hover {
+            text-decoration: none;
+        }
+
+        h1 {
+            font-size: 1.6rem;
+        }
+
+        main {
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            padding: 0 10%;
+        }
+    </style>
+
     @yield('head-extras')
 </head>
 <body>
-    <main class="container-fluid" style="padding-top: 0;">
-        <nav>
-            <ul>
-                <li><strong>Aragun LTDA</strong></li>
-            </ul>
-            <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Productos</a></li>
-                <li><a href="#" role="button">Iniciar sesión</a></li>
-            </ul>
-        </nav>
+    <header>
+        <h1><a href="{{route('home.index')}}">ARAGUN</a></h1>
+
+        <ul>
+            <li><a href="{{route('home.login')}}">INICIA SESIÓN</a></li>
+        </ul>
+    </header>
+
+    <main>
         @yield('main-content')
     </main>
 
