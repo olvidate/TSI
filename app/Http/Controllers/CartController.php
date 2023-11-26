5038eb44e657bed6cf189d1294add35592a0eed5
benjamin-ortizc <benjamin.ortizc@icloud.com>
2023-11-26 01:20:45 -0300

    feat: create first methods for shopping cart controller & import config

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Gloudemans\Shoppingcart\Facades\Cart;
+
+class CartController extends Controller
+{
+    public function index() {}
+
+    public function create() {}
+}
